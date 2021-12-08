ebb31bb6c92238025a110419cfdf8550cc2f3c9d
mre55 <misganawmihreteab>
2021-12-09 00:10:10 +0300

    Add test for remove function

--- src/localStorageOperations.test.js
+++ src/localStorageOperations.test.js
@@ -1,25 +1,44 @@
+import { JSDOM } from 'jsdom';
 import MockLSOperations from './__mocks__/localStorageOperations.js';
 import manipulateHtml from './manipulateHtml.js';
-import { JSDOM } from "jsdom"
-const dom = new JSDOM()
-global.document = dom.window.document
-global.window = dom.window
+
+const dom = new JSDOM();
+global.document = dom.window.document;
+global.window = dom.window;
 
 describe('Add Method', () => {
-  test('Add to localstorage', () => {
-    expect(MockLSOperations.add({target: {value: 'ABC'} })).toBe(1);
+  test('Add to the list', () => {
+    expect(MockLSOperations.add({ target: { value: 'ABC' } })).toBe(1);
   });
 
   test('Add one new item to the list', () => {
-    document.body.innerHTML =
-    `<ul id="todo-list">
+    document.body.innerHTML = `<ul id="todo-list">
       <li>Today's To Do<i class="fa-solid fa-arrows-rotate"></i></li>
       <li class="add-item"><input type="text" id="inp-add-item" placeholder="Add to your list ..."></li>
       <li class="btn"><a href="">Clear all Completed</a></li>
-    </ul>`
-    
-    manipulateHtml.addListItem({description: 'First Item', completed: false, index:1});
+    </ul>`;
+
+    manipulateHtml.addListItem({ description: 'First Item', completed: false, index: 1 });
     const list = document.querySelectorAll('#todo-list li');
     expect(list).toHaveLength(4);
+  });
 });
+
+describe('Remove Method', () => {
+  test('Remove from the list', () => {
+    expect(MockLSOperations.remove({ target: { index: '1' } })).toBe(2);
+  });
+
+  test('Remove one new item from the list', () => {
+    document.body.innerHTML = `<ul id="todo-list">
+      <li>Today's To Do<i class="fa-solid fa-arrows-rotate"></i></li>
+      <li class="add-item"><input type="text" id="inp-add-item" placeholder="Add to your list ..."></li>
+      <li class="btn"><a href="">Clear all Completed</a></li>
+    </ul>`;
+
+    MockLSOperations.displayList();
+    MockLSOperations.remove({ target: { index: '1' } });
+    const list = document.querySelectorAll('.todoItem');
+    expect(list).toHaveLength(1);
+  });
 });
